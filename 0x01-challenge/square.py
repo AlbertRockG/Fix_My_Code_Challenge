b71f16f4311dc2cd22a90c4bc740deddc0ffc94b
bioaddict <rafgangbadja@gmail.com>
2023-01-08 21:52:18 +0100

    Add documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""Defines a class named Square"""
+
 
 class square():
-    
+    """Represents a Square.
+
+    Args:
+        width (int): The width of the square.
+        height (int): The height of the height.
+    """
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """Initialize a new square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +23,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Return a printable of format of the square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
